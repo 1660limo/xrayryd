efe2267ce11cd0f1017087b5ce8a49db678ae5ed
ming <74036136+1660limo@users.noreply.github.com>
2022-07-10 17:21:44 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -175,7 +175,7 @@ install_XrayR() {
     if [[ ! -f /etc/XrayR/rulelist ]]; then
         cp rulelist /etc/XrayR/
     fi
-    curl -o /usr/bin/XrayR -Ls https://raw.githubusercontent.com/1660limo/XrayR-script/master/XrayR.sh
+    curl -o /usr/bin/XrayR -Ls https://raw.githubusercontent.com/1660limo/xrayryd/main/install.sh
     chmod +x /usr/bin/XrayR
     ln -s /usr/bin/XrayR /usr/bin/xrayr # 小写兼容
     chmod +x /usr/bin/xrayr
